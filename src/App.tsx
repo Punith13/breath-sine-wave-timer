ef402644c6070285ff5dd263d04ded436453e4a9
Punith Nayak <punithnayak@Puniths-Mac-Studio.local>
2025-08-04 15:45:50 +1000

    fix type issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import  { useRef, useEffect, useState } from "react";
 
 const BreathTimer = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
